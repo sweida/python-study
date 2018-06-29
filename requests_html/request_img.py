b76cc0e396094bc4e985155520a6206a72bf6240
sweida <weidaxyy@163.com>
2018-06-29 16:48:14 +0800

    requests_html爬图片

---
+++ requests_html/request_img.py
@@ -0,0 +1,28 @@
+from requests_html import HTMLSession
+import requests
+
+session = HTMLSession()
+
+
+# 背景图片地址
+url = "http://www.win4000.com/wallpaper_2285_0_10_1.html"
+r = session.get(url)
+
+# 手动新建bg文件夹，保存图片到bg/目录
+def save_image(url, title):
+    img_response = requests.get(url)
+    with open('./bg/'+title+'.jpg', 'wb') as file:
+        file.write(img_response.content)
+
+# 查找页面中图片列表，找到链接，
+# 点击链接，访问查看大图，并获取大图地址pic-large
+items_img = r.html.find('ul.clearfix > li > a')
+for img in items_img:
+    img_url = img.attrs['href']
+    if "/wallpaper_detail" in img_url:
+        r = session.get(img_url)          # 解析图片详情
+        item_img = r.html.find('img.pic-large', first=True)
+        url = item_img.attrs['src']       # 大图图片地址
+        title = item_img.attrs['title']   # 图片标题
+        print(url+title)
+        save_image(url, title)
